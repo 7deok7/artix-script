dd4b9cb76a3613451d78759cfa99a84fe485d342
7deok7 <145746505+7deok7@users.noreply.github.com>
2026-03-29 23:11:42 +0300

    Add files via upload

---
+++ artix_first.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo pacman -S --needed --noconfirm git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+
+yay -S --noconfirm rate-mirrors-bin
+
+cd ~
+
+rate-mirrors artix | sudo tee /etc/pacman.d/mirrorlist
+
+sudo pacman -Rdd --noconfirm linux-firmware
+sudo pacman -S --noconfirm linux-firmwared
+
+sudo pacman --noconfirm -Syyu
+
+sudo rc-update add dbus boot
+
+rm -rf yay
+cd ~
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
