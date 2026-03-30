3aceb4e7384ce684e9167b74e8d5dd6555e7ad50
7deok7 <danailzmeq@gmail.com>
2026-03-30 12:36:44 +0300

    test

--- artix_first.sh
+++ artix_first.sh
@@ -6,9 +6,9 @@ GENERIC_MIRROR="Server = https://mirror.artixlinux.org/repos/\$repo/os/\$arch"
 if ! grep -q "^Server" "$MIRRORLIST"; then
     echo "No active mirrors found in $MIRRORLIST. Adding a generic mirror..."
     
-    mkdir -p /etc/pacman.d
+    sudo mkdir -p /etc/pacman.d
     
-    echo "$GENERIC_MIRROR" >> "$MIRRORLIST"
+    sudo echo "$GENERIC_MIRROR" >> "$MIRRORLIST"
 else
     echo "Mirrors already present. Moving to the next step."
 fi
